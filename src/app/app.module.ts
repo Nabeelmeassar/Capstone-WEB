09ad4e3557d994ac2a3c9de6eb42882efe733c9f
Nabeel <nabeel@Nabeels-MBP.fritz.box>
2019-11-13 13:39:36 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PostComponent } from './compnents/post/post.component';
 import { PostsService} from './services/posts.service';
-import { HomeComponent } from './home/home.component';
-import {ContactUsComponent} from './compnents/contact-us/contact-us.component';
 // @ts-ignore
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    HomeComponent,
-    ContactUsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
